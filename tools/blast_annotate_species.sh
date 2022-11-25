5cdbf46c6d725d2edcb3aec1e66ca9919b2e9aff
ssswzh <aristazhang521@outlook.com>
2022-11-25 18:23:00 +0800

    minor change

--- tools/blast_annotate_species.sh
+++ tools/blast_annotate_species.sh
@@ -16,16 +16,26 @@ outfile=$3
 if [ -f ${db} ]
 then dir=`dirname ${db}`
     if [ ! -f ${db}.nin ]
-    then makeblastdb -in ${db} -dbtype nucl -title ${db} -out ${db}
+    then echo "makeblastdb -in ${db} -dbtype nucl -title ${db} -out ${db}"
     fi
-else echo "Database not found"
-    exit 
+else if [ ! -f ${BLASTDB}/${db} ]
+        then echo "Database not found in both `pwd` and BLASTDB=${BLASTDB}"
+             echo "makeblastdb -in ${db} -dbtype nucl -title ${db} -out ${db}"
+             exit 
+    fi
 fi
 
 printf "blastn -db ${db} -query ${fasta} -evalue 1e-5 -outfmt 7 -num_threads 8 -out ${outfile}.blast \n\n"
 
 printf "grep -v \"#\" ${outfile}.blast|awk 'BEGIN{FS=\"\\\t\";OFS=\"\\\t\"}{if(a[\$1]){if(a[\$1]<\$12){a[\$1]=\$12;b[\$1]=\$2;c[\$1]=\$0}}else{a[\$1]=\$12;b[\$1]=\$2;c[\$1]=\$0}}END{for(i in a){print c[i]}}' > ${outfile}.blast.besthit \n\n"
 
-#printf "awk 'NR==FNR{a[\$1]=\$0} NR>FNR{if(a[\">\"\$2]){print \$0\"\\\t\"a[\"\>\"\$2]} else{print \$0\"\\\tNot found\"} }' /export/home/zhangsiwen/database/nt/nt_20170615.fa.species ${outfile}.blast.besthit > ${outfile}.blast.besthit.species \n"
-
-#printf "cut -f13 ${outfile}.blast.besthit.species|cut -d' ' -f2-3|sed 's/ /_/g'|sed 's/[,]//g'|sort|uniq -c|sort -k1,1nr|awk '{print $2"\t"$1}' > ${outfile}.blast.besthit.species.counts \n"
+if [ -f ${db}.species ]
+then
+    printf "awk 'NR==FNR{if(\$1~/^>/){gsub(\">\",\"\",\$1)} a[\$1]=\$0} NR>FNR{if(a[\$2]){print \$0\"\\\t\"a[\$2]} else{print \$0\"\\\tNot found\"} }' ${db}.species ${outfile}.blast.besthit > ${outfile}.blast.besthit.species \n\n"
+    printf "cut -f13- ${outfile}.blast.besthit.species|cut -d' ' -f2-3|sed 's/ /_/g'|sed 's/[,]//g'|sort|uniq -c|sort -k1,1nr|awk '{print \$2\"\\\t\"\$1}' > ${outfile}.blast.besthit.species.counts \n"
+fi
+if [ -f ${BLASTDB}/${db} ]
+then
+    printf "awk 'NR==FNR{if(\$1~/^>/){gsub(\">\",\"\",\$1)} a[\$1]=\$0} NR>FNR{if(a["\$2]){print \$0\"\\\t\"a[\$2]} else{print \$0\"\\\tNot found\"} }' ${BLASTDB}/${db}.species ${outfile}.blast.besthit > ${outfile}.blast.besthit.species \n\n"
+    printf "cut -f13- ${outfile}.blast.besthit.species|cut -d' ' -f2-3|sed 's/ /_/g'|sed 's/[,]//g'|sort|uniq -c|sort -k1,1nr|awk '{print \$2\"\\\t\"\$1}' > ${outfile}.blast.besthit.species.counts \n"
+fi
